0a367c09cf8e5b7324eb514d4a1655798b79d5a2
Andrey <andrew1000@list.ru>
2017-05-03 16:09:38 +0300

    added draw block func to block file and its header file

--- AllegroTetris/src/block.h
+++ AllegroTetris/src/block.h
@@ -3,16 +3,17 @@
 
 #include "allegro_framework.h"
 
-typedef struct 
-{
-    int type;   
-    int color;  
-    int x, y;   
-    int dir;    
-    int data;   
+typedef struct {
+    int type;   // what kind of tetromino (e.g. I, O, L...)
+    int color;  // what color index the block has
+    int x, y;   // location in the playfield
+    int dir;    // used for rotating the block
+    int data;   // data about the block shape (see block_data)
 } Block;
 
 void generate_block_at(Block *block, int x, int y);
 void rotate_block_left(Block *block);
 void rotate_block_right(Block *block);
+void draw_block(Block *block);
+
 #endif
