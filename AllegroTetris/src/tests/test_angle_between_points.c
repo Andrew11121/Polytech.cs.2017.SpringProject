50d5a0ec5d94fd72ca2b1d7e916fcd3a8c8846ba
Andrey <andrew1000@list.ru>
2017-05-29 21:25:12 +0300

    made 2 tests compare results

--- AllegroTetris/src/tests/test_angle_between_points.c
+++ AllegroTetris/src/tests/test_angle_between_points.c
@@ -16,15 +16,14 @@ TEST_SETUP(TestAngleBetweenPoints)
 TEST_TEAR_DOWN(TestAngleBetweenPoints)
 {
 }
-
+//testing angle between points: comparing expected result with a result that we get from the func
 TEST(TestAngleBetweenPoints, FunctionAngleBetweenPoints)
 
 {
+float x1 = 0, x2 = 0, y1 = 0, y2 = 0, a, b;
+a = 0;
+b = atan2(y2 - y1, x2 - x1);
 
-float x1, x2, y1, y2;
-
-return atan2(y1 - y2, x1 - x2);
-
-TEST_ASSERT_EQUAL_INT_ARRAY( 100, 100, 100);
+TEST_ASSERT_EQUAL_FLOAT(a, b);
 }
 
