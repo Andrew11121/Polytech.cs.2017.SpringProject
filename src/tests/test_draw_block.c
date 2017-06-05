7b17f5bcf4c7f281589dad7179cfa3368b162e18
alievilya <aliev.ilia@yandex.ru>
2017-06-05 23:19:18 +0200

    fixed test

--- src/tests/test_draw_block.c
+++ src/tests/test_draw_block.c
@@ -21,28 +21,23 @@ TEST_TEAR_DOWN(TestDrawBlock)
 /** \fn TEST(TestDrawBlock, FunctionDrawBlock) 
 тестирование вывода блока на экран: сравнение ожидаемого результата из теста с результатом из программы с текущими значениями 
 */
+
 TEST(TestDrawBlock, FunctionDrawBlock)
 
 {
 
-int a=0;
+int a=0, x1, y1, x0, y0, x, y;
 Block *block;
-   for (int y = null_coord; y < 4; y++) 
+   for (y = 0; y < 4; y++) 
    {
-        for (int x = null_coord; x < 4; x++) 
+        for (x = 0; x < 4; x++) 
       {
-            if (block->data & (1 << ((y * 4) + x))) 
-	 {
-                int x0 = (block->x + OFFSET_X + x) * BLOCK_SIZE;
-                int y0 = (block->y - OFFSET_Y + y) * BLOCK_SIZE;
-                int x1 = x0 + BLOCK_SIZE;
-                int y1 = y0 + BLOCK_SIZE;
-                if (x1 < 0 || x0 < 0 || y1 < 0 || y0<0)
-                return a=3;
-         }
+            x1 = x0 + 32;
+            y1 = y0 + 32;
       }
     }
 
-TEST_ASSERT_EQUAL_INT_ARRAY(a, 0, 100);
+if (x1 < 0 || y1 < 0 || x < 0 || y < 0)
+  a = 3;
+TEST_ASSERT_EQUAL_INT_ARRAY(a, 3, 100);
 }
-
